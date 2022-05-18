2bc6ed1735c641f7d6ba7ba9974f7e25fa2b0c59
ZIHUA HUANG <zhuang4@scu.edu>
2022-05-18 11:12:32 -0700

    before test, merge Zijie

--- src/main/java/com/itlize/Joole/service/serviceImpl/ProjectManage.java
+++ src/main/java/com/itlize/Joole/service/serviceImpl/ProjectManage.java
@@ -1,7 +1,9 @@
 package com.itlize.Joole.service.serviceImpl;
 
+import com.itlize.Joole.entity.Product;
 import com.itlize.Joole.entity.Project;
 import com.itlize.Joole.entity.ProjectProduct;
+import com.itlize.Joole.repository.ProductRepository;
 import com.itlize.Joole.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,16 +15,18 @@ import java.util.Set;
 public class ProjectManage {
 
     @Autowired ProjectRepository projectRep;
+    @Autowired
+    ProductRepository productRepository;
 
     public int addProductToProject(int productId, int projectId)
     {
         Set<ProjectProduct> ppSet = new HashSet<ProjectProduct>();
 
-
+        Product product=productRepository.findById(productId).orElse(null);
         Project project = projectRep.findById(projectId).orElse(null);
 
         ProjectProduct pp = new ProjectProduct();
-
+        pp.setProduct(product);
         pp.setProject(project);
 
         ppSet.add(pp);
