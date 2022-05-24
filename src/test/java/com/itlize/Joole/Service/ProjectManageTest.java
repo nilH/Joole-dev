04a38fda8f273b9133f3c4f32226f47adfc0a8e4
ZIHUA HUANG <zhuang4@scu.edu>
2022-05-24 16:19:40 -0700

    copy 3 files from zijie

--- src/test/java/com/itlize/Joole/Service/ProjectManageTest.java
+++ src/test/java/com/itlize/Joole/Service/ProjectManageTest.java
@@ -1,4 +1,76 @@
 package com.itlize.Joole.Service;
 
+import com.itlize.Joole.entity.Product;
+import com.itlize.Joole.entity.Project;
+import com.itlize.Joole.entity.ProjectProduct;
+import com.itlize.Joole.entity.User;
+import com.itlize.Joole.repository.ProductRepository;
+import com.itlize.Joole.repository.ProjectProductRepository;
+import com.itlize.Joole.repository.ProjectRepository;
+import com.itlize.Joole.service.MyUserDetailService;
+import com.itlize.Joole.service.serviceImpl.ProjectManageImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class ProjectManageTest {
+
+    @Mock
+    ProductRepository productRepository;
+    @Mock
+    ProjectRepository projectRep;
+    @Mock
+    ProjectProductRepository ppRepository;
+
+    @Autowired
+    @InjectMocks
+    private ProjectManageImpl projectManage;
+    private Product product;
+
+    private Project project;
+
+    ProjectProduct pp;
+
+    @BeforeEach
+    public void setUp() {
+        Set<ProjectProduct> ppSet = new HashSet<ProjectProduct>();
+        product = new Product();
+        product.setProductName("A");
+        product.setProductId(1);
+        project = new Project();
+        project.setProjectName("B");
+        project.setId(2);
+        pp = new ProjectProduct();
+        ppSet.add(pp);
+        pp.setProduct(product);
+        pp.setProject(project);
+        project.setProjectProduct(ppSet);
+        product.setProjectProductSet(ppSet);
+    }
+
+
+
+    @Test
+    public void addProductToProjectTest()
+    {
+        when(productRepository.findById(any())).thenReturn(Optional.ofNullable(product));
+        when(projectRep.findById(any())).thenReturn(Optional.ofNullable(project));
+        when(ppRepository.save(any())).thenReturn(pp);
+
+        assertThat(projectManage.addProductToProject(1,2)).isEqualTo(1);
+    }
 }
--- src/test/java/com/itlize/Joole/Service/UserServiceTest.java
+++ src/test/java/com/itlize/Joole/Service/UserServiceTest.java
@@ -1,4 +1,83 @@
 package com.itlize.Joole.Service;
 
+import com.itlize.Joole.entity.User;
+import com.itlize.Joole.repository.UserRepository;
+import com.itlize.Joole.service.serviceImpl.UserServiceImpl;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder() ;
+
+    @Autowired
+    @InjectMocks
+    private UserServiceImpl userService;
+    private User user;
+    List<User> userList;
+
+    @BeforeEach
+    public void setUp() {
+        userList = new ArrayList<>();
+        user = new User();
+        user.setId(1);
+        user.setName("Oliver");
+        user.setPassword("123456");
+        userList.add(user);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        user = null;
+        userList = null;
+    }
+
+    @Test
+    public void saveUserTest() {
+        when(userRepository.save(user)).thenReturn(user);
+        when(passwordEncoder.encode(user.getPassword())).thenReturn("xxxx");
+        User tmpUser = userService.saveUser(user);
+        int id = tmpUser.getId();
+        assertEquals(id, 1);
+    }
+
+    @Test
+    public void findByUsernameTest(){
+        when(userRepository.findByName("Oliver")).thenReturn(Optional.ofNullable(user));
+        assertThat(userService.findByUsername("Oliver")).isEqualTo(user);
+    }
+
+    @Test
+    public void findAllUsersTest(){
+        when(userRepository.findAll()).thenReturn(userList);
+        List<User> userList1 =userService.findAllUsers();
+        assertEquals(userList1,userList);
+    }
+
+
+
+
 }
